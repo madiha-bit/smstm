0d938ef94a9ee110e04c7fdc525ea85e4805d4de
Dushyant Vishwakarma <dushyantss8@gmail.com>
2020-11-11 11:10:47 +0530

    Initial Commit

---
+++ about.php
@@ -0,0 +1,44 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                                    To emerge as one of the leading institute in the field of technical education to produce competent & committed professionals for the betterment of society.
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                   To create SISTec, as a centre of excellence for imparting technical & scientific knowledge through rigoruos academic, technological inteventions, skilled faculty, character building approach and entrepreneur mindset for the betterment of society.
+
+                                   <!-- <h1>WHY CHOOSE SISTec?</h1>
+                                   <ul>
+                                    <li>Quality education is an indispensable part of SISTec; the highly skilled faculties proactively work in cultivating technical and soft skills in budding engineers, pharmacists and managers.</li>
+                                    <li>Fully furnished labs, well equipped with latest technologies, engaged in 10000+ practical hours on IoT & Embedded Systems, Mechatronics, etc.</li>
+                                    <li>Winter and Summer Training Programs where Lab and On-Field hands-on training is given to keep up with the industry requirements.</li>
+                                    <li>Learning Resource Centre (Library) with 45000+ books, necessary required study material, national and international journals, magazines and newspapers.</li>
+                                    <li>Campus Recruitment Trainings from first year onwards in the regular timetable by expert corporate trainers.</li>
+                                    <li>Team of corporate mentors dedicatedly working on Training and Placements to generate the best of placement opportunities making it highest placements college in bhopal.</li>
+                                    <li>Technical and non-technical student clubs such as Student Activity Council (SAC), SISTec Mechanical Engineers Association (SMEA), Federal Association of Civil Engineer (F.A.C.E.), National Cadet Corps (NCC), Computer Society of India (CSI), etc.</li>
+                                    <li>Series of events and activities to bring out both the corporate and cultural aspect of students like Sagar Manthan, SISTec Neutron, Nirmaan, Sagar Fiesta, SISTec Start-up Meet, Tech Talks, Celebrity Nights and many more.</li>
+                                   </ul> -->
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
---
+++ contact.php
@@ -0,0 +1,31 @@
+ <!-- Map Column -->
+            <div class="col-md-8">
+ School Location
+                <!-- Embedded Google Map -->
+             <div class="mapouter"><div class="gmap_canvas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117312.26050535403!2d77.25067451768919!3d23.24279082709208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c6650df25ad15%3A0x50c6581fbb7c9940!2sSagar%20Group%20of%20Institutions%20-%20SISTec!5e0!3m2!1sen!2sin!4v1600664766655!5m2!1sen!2sin" width="500" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe><a href="https://www.pureblack.de">pureblack.de</a></div><style>.mapouter{text-align:right;height:600px;width:400px;}.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:600px;}</style></div>
+        </div> 
+            <!-- Contact Details Column -->
+            <div class="col-md-4">
+                <h3>Contact Details</h3>
+                <p> 
+                <br>Sagar Group of Institutions-SISTec, opposite International Airport, Gandhi Nagar, Bhopal, Madhya Pradesh 462036<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>:+919109975760</p>
+                
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Sunday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="https://www.facebook.com/sagargroupofinstitutionssistec/" target="blank"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://in.linkedin.com/school/sagar-institute-of-science-technology-and-engineer/" target="blank"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/sagarcollege?lang=en" target="blank"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                 </ul>
+            </div>
+
+ 
